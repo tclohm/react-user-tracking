048a0f4ff9067a1f3f81d82fa01951e64a0256b6
Taylor Lohman <taylorlohman@duck.com>
2025-05-06 15:21:54 -0700

    oops

--- src/TrackingToast.tsx
+++ src/TrackingToast.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useTracking } from './TrackingContext';
-import '/index.css';
+import './index.css';
 
 interface TrackingToastProps {
   privacyUrl?: string;
